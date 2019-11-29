a482ac82a214f5c34b1b031660e68d74ff2c2c06
formando <formando@c2-lcm-M11.local>
2019-11-29 15:47:19 +0000

    konsta

--- CustomLogin/ViewControllers/ProfileViewController.swift
+++ CustomLogin/ViewControllers/ProfileViewController.swift
@@ -11,7 +11,7 @@ import Firebase
 import FirebaseFirestore
 import FirebaseAuth
 
-class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     //MARK: Properties
     
@@ -51,6 +51,8 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         super.viewDidLoad()
         
         
+        
+        
         userCollectionRef = Firestore.firestore().collection("users")
    
 
@@ -86,6 +88,28 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     
     //MARK: Actions
+
+    
+    // edit profile informations
+    @IBAction func usernameEdit(_ sender: UITapGestureRecognizer) {
+        
+        let userEdit = UITextField(frame: CGRect(x: 20.0, y: 30.0, width: 100.0, height: 33.0))
+        userEdit.backgroundColor = UIColor.red
+        userEdit.borderStyle = UITextField.BorderStyle.line
+        self.view.addSubview(userEdit)
+    
+    }
+    
+    @IBAction func firstnameEdit(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func lastnameEdit(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func emailEdit(_ sender: UITapGestureRecognizer) {
+    }
+    
+    //change user photo
     
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
         
@@ -118,7 +142,57 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         dismiss(animated: true, completion: nil)
     }
     
+    // MARK:- ---> UITextFieldDelegate
     
+        func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+            // return NO to disallow editing.
+            print("TextField should begin editing method called")
+            return true
+        }
+
+        func textFieldDidBeginEditing(_ textField: UITextField) {
+            // became first responder
+            print("TextField did begin editing method called")
+        }
+
+        func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+            // return YES to allow editing to stop and to resign first responder status. NO to disallow the editing session to end
+            print("TextField should snd editing method called")
+            return true
+        }
+
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            // may be called if forced even if shouldEndEditing returns NO (e.g. view removed from window) or endEditing:YES called
+            print("TextField did end editing method called")
+        }
+
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+            // if implemented, called in place of textFieldDidEndEditing:
+            print("TextField did end editing with reason method called")
+        }
+
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            // return NO to not change text
+            print("While entering the characters this method gets called")
+            return true
+        }
+
+        func textFieldShouldClear(_ textField: UITextField) -> Bool {
+            // called when clear button pressed. return NO to ignore (no notifications)
+            print("TextField should clear method called")
+            return true
+        }
+
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            // called when 'return' key pressed. return NO to ignore.
+            print("TextField should return method called")
+            // may be useful: textField.resignFirstResponder()
+            return true
+        }
+
+    
+
+    // MARK: UITextFieldDelegate <---
     
     /*
     // MARK: - Navigation
