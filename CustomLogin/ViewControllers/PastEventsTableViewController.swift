2855d040006b2bf69b0d6d2c31cfe2128dcb37fc
xavi <xavi>
2020-01-15 17:20:33 +0000

    diahane

--- CustomLogin/ViewControllers/PastEventsTableViewController.swift
+++ CustomLogin/ViewControllers/PastEventsTableViewController.swift
@@ -12,16 +12,26 @@ import FirebaseFirestore
 import FirebaseAuth
 
 class PastEventsTableViewController: UITableViewController, UISearchResultsUpdating, UISearchBarDelegate {
+    
+    //update search results while insert
    func updateSearchResults(for searchController: UISearchController) {
+    
         filteredTableData.removeAll(keepingCapacity: false)
 
         let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
         let array = (tableData as NSArray).filtered(using: searchPredicate)
         filteredTableData = array as! [String]
         searchController.searchResultsController?.view.isHidden = false
+    
         self.tableView.reloadData()
     }
- 
+    
+    //show table data when search bar empty
+    func willPresentSearchController(for searchController :UISearchController) {
+           searchController.searchResultsController?.view.isHidden = false
+            self.tableView.reloadData()
+    }
+    
     var finalName = "Patrik"
     var tableData = [String]()
     var userCollectionRef: CollectionReference!
@@ -39,6 +49,7 @@ class PastEventsTableViewController: UITableViewController, UISearchResultsUpdat
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 45/255, green: 40/255, blue: 62/255, alpha: 1.0)
+        //add search bar
             resultSearchController = ({
                 let controller = UISearchController(searchResultsController: nil)
                 controller.searchResultsUpdater = self
@@ -85,17 +96,14 @@ class PastEventsTableViewController: UITableViewController, UISearchResultsUpdat
                             self.myLon.append(longitude!)
                                print(name)
                            }
-                        
-                        
                        }
-                      
                        
                        // hide progress view
                        self.tableView.reloadData()
                        activityIndicatorView.stopAnimating()
                    }
                }
-            // Reload the table
+        self.tableView.reloadData()
         }
 
     // MARK: - Table view data source
@@ -103,26 +111,32 @@ class PastEventsTableViewController: UITableViewController, UISearchResultsUpdat
    override func numberOfSections(in tableView: UITableView) -> Int {
            // #warning Incomplete implementation, return the number of sections
            return 1
-           
        }
-
 
        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           // #warning Incomplete implementation, return the number of rows
-           return tableData.count
+            // return the number of rows
+            if  (resultSearchController.isActive) {
+                return filteredTableData.count
+            } else {
+                return tableData.count
+            }
        }
 
        
-       override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
            
-           let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-       
-           let(events) = tableData[indexPath.row]
-           cell.textLabel?.text = events
-
-        return cell
-    
-       }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+      
+        if (resultSearchController.isActive) {
+            cell.textLabel?.text = filteredTableData[indexPath.row]
+            return cell
+        }
+        else {
+            cell.textLabel?.text = tableData[indexPath.row]
+            print(tableData[indexPath.row])
+            return cell
+        }
+    }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath.row)
@@ -146,52 +160,6 @@ class PastEventsTableViewController: UITableViewController, UISearchResultsUpdat
         }
     }
 
-    
-   
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     func getCurrentTime() -> String{
         let format = DateFormatter()
         format.dateFormat = "MM-dd-yyyy' 'HH:mm"
@@ -199,5 +167,4 @@ class PastEventsTableViewController: UITableViewController, UISearchResultsUpdat
         let formattedDate = format.string(from: date)
         return formattedDate
     }
-
 }
