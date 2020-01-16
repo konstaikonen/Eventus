9076e15ac92734235592d093aa5d0206fba15162
Javier <javierporta@hotmail.com>
2020-01-16 15:25:00 +0000

    update

--- CustomLogin/ViewControllers/EditProfileViewController.swift
+++ CustomLogin/ViewControllers/EditProfileViewController.swift
@@ -61,8 +61,6 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //profilepicture round
-        
         //hide keyboard when click outside
         let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
             view.addGestureRecognizer(tap)
